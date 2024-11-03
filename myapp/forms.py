3b040ab3cb7e70d8ae87748d3b51ffe121777c8d
luismartinezkyb <18030545@itcelaya.edu.mx>
2024-11-03 14:47:23 -0600

    (feat): Styles de attributes on forms python

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class CreateNewTask(forms.Form):
-  title = forms.CharField(label="Task title", max_length=200)
-  description = forms.CharField(widget=forms.Textarea, label="Description",)
+  title = forms.CharField(label="Task title", max_length=200, widget=forms.TextInput(attrs={'class':'input'}))
+  description = forms.CharField(label="Description",widget=forms.Textarea(attrs={'class':'input'}))
   
 class CreateNewProject(forms.Form):
-  name = forms.CharField(label="Project Name", max_length=200)
+  name = forms.CharField(label="Project Name", max_length=200, widget=forms.TextInput(attrs={'class':'input'}))
